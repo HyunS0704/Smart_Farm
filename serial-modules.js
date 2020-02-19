4831c63130b3b10037be4ec00a01ea83db1b2608
Hyunseok Jeon <kimx31@naver.com>
2020-02-19 17:23:46 +0900

    sensor & actuator

--- serial-modules.js
+++ serial-modules.js
@@ -1,14 +1,13 @@
-const SerialPort = require("serialport");
-const Readline = require('@serialport/parser-readline');
-const sp = new SerialPort("COM5", {     // /dev/ttyACM0(라즈베리 파이에서 오픈경우)
-    baudRate: 115200,
-    autoOPen: false
-});
-
-const spp = sp.pipe(new Readline()); 
-
 module.exports = {
     readSensor: function(callback) {
+        const SerialPort = require("serialport");
+        const Readline = require('@serialport/parser-readline');
+        const sp = new SerialPort("COM5", {     // /dev/ttyACM0(라즈베리 파이에서 오픈경우)
+            baudRate: 115200,
+            autoOPen: false
+        });
+
+        const spp = sp.pipe(new Readline()); 
         var readyFlag = true;
         sp.open(function() {
             console.log('Serial Port OPEN');
@@ -39,6 +38,13 @@ module.exports = {
         });
     },
     writeActuator: function(jsonData, callback) {
+        const SerialPort = require("serialport");
+        const Readline = require('@serialport/parser-readline');
+        const sp = new SerialPort("COM5", {     // /dev/ttyACM0(라즈베리 파이에서 오픈경우)
+            baudRate: 115200,
+            autoOPen: false
+        });
+        const spp = sp.pipe(new Readline()); 
         let message = `PUT ${jsonData}\n`;
         sp.open(function() {                          //또는 sp.open(funtion() {...})
             console.log('Serial Port OPEN');                //or sp.on('open', function() { 
